13324cc262da4c6960d35835cca30e74fcd482f3
Trubo4ist <applepi2001@mail.ru>
2019-04-18 16:56:12 +0300

    Ready project

---
+++ Prakt1/ClassIzd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prakt1
+{
+    class ClassIzd
+    {
+        public string Name;
+        public string Shifr;
+        public int Count;
+        private string v1;
+        private string v2;
+        private int v3;
+        public ClassIzd(string v1, string v2, int v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+
+        public ClassIzd()
+        {
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Название: {Name} Шифр: {Shifr} Количество: {Count} ");
+        }
+    }
+}
